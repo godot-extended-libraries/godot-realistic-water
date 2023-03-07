9ff45ddb11b2f2de99a65a522a221f234c5c4115
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-03-07 10:54:17 -0800

    Restore shaders. No paramters.

--- realistic_water_shader/core/camera.gd
+++ realistic_water_shader/core/camera.gd
@@ -1,4 +1,4 @@
-extends Spatial
+extends Node3D
 
 var camera_position = Vector3()
 var camera_angle = 35.0
@@ -21,7 +21,7 @@ func _input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	
-	var camera_aim = $Camera.get_global_transform().basis
+	var camera_aim = $Camera3D.get_global_transform().basis
 	var camera_direction = Vector3()
 	
 	if Input.is_action_pressed("player_up"):
@@ -40,13 +40,13 @@ func _process(delta: float) -> void:
 	self.global_transform.origin +=  camera_direction * camera_speed * delta
 
 	camera_distance = clamp(camera_distance, camera_distance_min, camera_distance_max)
-	$Camera.transform.origin.y = sin(deg2rad(camera_angle)) * camera_distance
-	$Camera.transform.origin.z = cos(deg2rad(camera_angle)) * camera_distance
+	$Camera3D.transform.origin.y = sin(deg_to_rad(camera_angle)) * camera_distance
+	$Camera3D.transform.origin.z = cos(deg_to_rad(camera_angle)) * camera_distance
 	
 	if Input.is_action_pressed("player_angle"):
 		camera_angle += mouse_delta_position.y * 0.25
 		camera_angle = clamp(camera_angle, 35.0, 85.0)
-		$Camera.transform = $Camera.transform.looking_at(Vector3(0.0, 0.0, 0.0), Vector3(0.0, 1.0, 0.0))
+		$Camera3D.transform = $Camera3D.transform.looking_at(Vector3(0.0, 0.0, 0.0), Vector3(0.0, 1.0, 0.0))
 		self.rotate_y(-mouse_delta_position.x * 0.01)
 	
 	mouse_delta_position = Vector2()	
